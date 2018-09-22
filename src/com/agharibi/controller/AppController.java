747e4e0795b9238a42f3ffc526449da234c4b2df
Armen G <gharibi.armen@yahoo.com>
2018-09-22 13:34:17 -0700

    Added annotation based config instead of xml based annotation

--- src/com/agharibi/controller/AppController.java
+++ src/com/agharibi/controller/AppController.java
@@ -3,11 +3,12 @@ package com.agharibi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.agharibi.config.AppConfig;
 import com.agharibi.dao.AppDaoImpl;
 import com.agharibi.model.User;
 
@@ -18,8 +19,7 @@ public class AppController {
 	public ModelAndView homePage() {
 		List<User> users = new ArrayList<>();
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"/com/agharibi/dao/Spring-AppDAOConfig.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		AppDaoImpl appDao = context.getBean("DAOBean", AppDaoImpl.class);
 		users = appDao.listUsers();
 		System.out.println(users);
