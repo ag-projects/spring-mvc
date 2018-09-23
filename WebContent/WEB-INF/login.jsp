5925593dc8c3a52c04121804aa26d472174efd6a
Armen G <gharibi.armen@yahoo.com>
2018-09-23 15:38:30 -0700

    Added remmeber me using annotations

--- WebContent/WEB-INF/login.jsp
+++ WebContent/WEB-INF/login.jsp
@@ -1,21 +1,21 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
 <title>Login Page</title>
 </head>
 <body onload='document.f.username.focus();'>
 	<hr />
-		Custom Form
+	Custom Form
 	<hr />
 
 	<h3>Login with Username and Password</h3>
 	<form name='f' action='${loginUrl}' method='POST'>
 		<table>
-		
-		<c:if test="${param.error != null}">
-			<span  style="color:red" >Invalid Credentials</span>
-		</c:if>
-		
+
+			<c:if test="${param.error != null}">
+				<span style="color: red">Invalid Credentials</span>
+			</c:if>
+
 			<tr>
 				<td>User:</td>
 				<td><input type='text' name='username' value=''></td>
@@ -23,6 +23,11 @@
 			<tr>
 				<td>Password:</td>
 				<td><input type='password' name='password' /></td>
+			</tr>
+			<tr>
+				<td>Remember me:</td>
+				<td><input name="remember-me" id="remember-me" type="checkbox"
+					checked="checked" /></td>
 			</tr>
 			<tr>
 				<td colspan='2'><input name="submit" type="submit"
--- src/com/agharibi/config/SecurityConfig.java
+++ src/com/agharibi/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -15,22 +17,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(this.dataSource);
 	}
-	
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
-		.antMatchers("/user").access("hasRole('ROLE_USER')");
-		
+		http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')").antMatchers("/user")
+				.access("hasRole('ROLE_USER')");
+
 		http.formLogin().loginPage("/login").failureUrl("/login?error");
+
+		http.rememberMe().rememberMeParameter("remember-me").tokenValiditySeconds(50000).key("anyKey").tokenRepository(tokenRepository());
 	}
-	
-	
+
+	private PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(this.dataSource);
+		return tokenRepository;
+	}
 
 }
